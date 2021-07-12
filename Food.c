2b0028914afd1625581b2b408ee7fc96665ab197
pranitha23102001 <74504768+pranitha23102001@users.noreply.github.com>
2021-07-12 22:57:09 +0530

    Create Food.c

---
+++ Food.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+int main()
+{
+	int choice;
+	printf("--------------MENU---------------\n");
+	printf("1. Pizza, Rs. 239\n");
+	printf("2. Burger, Rs. 129\n");
+	printf("3. Pasta, Rs. 179\n");
+	printf("4. French Fries, Rs. 99\n");
+	printf("5. Sandwich, Rs. 149\n");
+	printf("---------------------------------\n");
+	printf("\nEnter your choice: ");
+	scanf("%d", &choice);
+	switch(choice)
+	{
+		case 1:
+			printf("Food name: Pizza\nPrice: Rs. 239");
+			break;
+		case 2:
+			printf("Food name: Burger\nPrice: Rs. 129");
+			break;
+		case 3:
+			printf("Food name: Pasta\nPrice: Rs. 179");
+			break;
+		case 4:
+			printf("Food name: French Fries\nPrice: Rs. 99");
+			break;
+		case 5:
+			printf("Food name: Sandwich\nPrice: Rs. 149");
+			break;
+		default:
+			printf("Wrong input (Please choose between 1-5)");
+	}
+	
+	return 0;
+}
